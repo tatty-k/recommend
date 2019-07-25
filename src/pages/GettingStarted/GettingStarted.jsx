5fe438a81073b37076132388a5c8a9710fe9f7cf
Tatty <tattyklass@gmail.com>
2019-07-24 17:12:57 -0700

    getting started touch-ups

--- src/pages/GettingStarted/GettingStarted.jsx
+++ src/pages/GettingStarted/GettingStarted.jsx
@@ -3,33 +3,29 @@ import './GettingStarted.css';
 
 const GettingStarted = () => {
     return (
-        <div className= "gs-grid-container">
+        <div className= "gs-container">
             
-            <div className="gs-item1 gs-grid-item ">
-                <div className="gs-top">1. Click the add group button to create your groups </div>  
-                <div className="gs-bottom">picture</div>
+            <div className="gs-item1 gs-item ">
+                1. Click the add group button to create your groups  
             </div>
 
-            <div className="gs-item2 gs-grid-item ">
-                
+            <div className="gs-item2 gs-item ">
+                picture
             </div>
 
-            <div className="gs-item3 gs-grid-item ">
-                
+            <div className="gs-item3 gs-item ">
+                2. Name your group and add members
             </div>
 
-            <div className="gs-item4 gs-grid-item ">
-                <div className="gs-top">2. Name your group and add members</div>
-                <div className="gs-bottom">picture</div>
+            <div className="gs-item4 gs-item ">
+                picture
             </div>
 
-            <div className="gs-item5 gs-grid-item ">
-                <div className="gs-top">3. Click on the group name to enter the group page</div>
-                <div className="gs-bottom">picture</div>
+            <div className="gs-item5 gs-item ">
+                3. Click on the group name to enter the group page
             </div>
-
-            <div className="gs-item6 gs-grid-item ">
-                
+            <div className="gs-item6 gs-item ">
+                picture
             </div>   
         </div>
      );
